b0d12a233c9a185661a3b3679ec7643bd472f84d
Niraj.Kumar25ae52 <niraj.kumar25ae52@cognizant.com>
2025-08-06 15:02:05 +0530

    Revert "Merge branch 'feature/home-favorites' into 'master'"
    This reverts merge request !6

--- src/app/models/filter-options.model.ts
+++
@@ -1,9 +0,0 @@
-export interface FilterOptions {
-  location: string;
-  checkIn?: Date;
-  checkOut?: Date;
-  minPrice: number;
-  maxPrice: number;
-  propertyTypes: string[];
-  amenities: string[];
-}
--- src/app/utils/date.utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatDateForInput(date: Date): string {
-  return date.toISOString().split('T')[0];
-}
-
-export function parseInputDate(dateString: string): Date {
-  return new Date(dateString);
-}
